a41dfa1bed155c3327dc575b154481b87b1bf6bf
antonBlagodarnyy <antonbgi98@gmail.com>
2026-01-13 13:23:36 +0100

    init del scrapper

---
+++ NoticiasScrapper/src/main/java/com/SinAnimoDeLucro/NoticiasScrapper/NoticiasScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.SinAnimoDeLucro.NoticiasScrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticiasScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticiasScrapperApplication.class, args);
+	}
+
+}
---
+++ NoticiasScrapper/src/test/java/com/SinAnimoDeLucro/NoticiasScrapper/NoticiasScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SinAnimoDeLucro.NoticiasScrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticiasScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
