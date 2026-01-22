9f1815003fe627f634f89242fdf52656e71d48b2
donatomarino <donato_8@icloud.com>
2026-01-22 10:15:59 +0100

    fix

---
+++ NoticiasApi/src/main/java/com/SinAnimoDeLucro/NoticiasApi/NoticiasApiApplication.java
@@ -0,0 +1,13 @@
+package com.SinAnimoDeLucro.NoticiasApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticiasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticiasApiApplication.class, args);
+	}
+
+}
