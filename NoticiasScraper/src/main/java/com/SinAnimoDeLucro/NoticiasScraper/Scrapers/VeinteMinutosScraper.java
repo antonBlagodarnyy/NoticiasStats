77891f52575f5f8fc1fa7a3f1c43625bc429e082
antonBlagodarnyy <antonbgi98@gmail.com>
2026-08-09 18:03:18 +0200

    Bugfix: 20min scraper obsolete

--- NoticiasScraper/src/main/java/com/SinAnimoDeLucro/NoticiasScraper/Scrapers/VeinteMinutosScraper.java
+++ NoticiasScraper/src/main/java/com/SinAnimoDeLucro/NoticiasScraper/Scrapers/VeinteMinutosScraper.java
@@ -22,74 +22,80 @@ import java.util.Locale;
 @Component("20Minutos")
 @Slf4j
 public class VeinteMinutosScraper implements Scraper {
-  @Autowired
-  private NewsPaperServiceImpl newsPaperService;
+    @Autowired
+    private NewsPaperServiceImpl newsPaperService;
 
-  @Autowired
-  private ArticleServiceImpl articleService;
+    @Autowired
+    private ArticleServiceImpl articleService;
 
-  @Override
-  public List<Article> scrape(String url) {
-    log.info("-----> [VeinteMinutosScraper] -> Leyendo el HTML del noticiero 20Mintutos... <-----");
-    Newspaper veinteMinutos = newsPaperService.findByName("20Minutos");
-    DateTimeFormatter dateFormat =
-      DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.forLanguageTag("es-ES"));
-    List<Article> newsToday = new ArrayList<>();
-
-    try {
-      Document doc = Jsoup.connect(url).get();
-      Elements newsItems = doc.select("article");
-      for (Element newsItem : newsItems) {
-        Element urlNewsEl = newsItem.selectFirst("a");
-        if(urlNewsEl == null) continue;
-
-        String urlNews = urlNewsEl.attr("abs:href");
-        if(urlNews.isBlank()) continue;
-
-        if (articleService.existsByUrl(urlNews)) {
-          log.debug("[VeinteMinutosScraper] -> Noticia duplicada, se ignora: {}", urlNews);
-          continue;
-        }
+    @Override
+    public List<Article> scrape(String url) {
+        log.info("-----> [VeinteMinutosScraper] -> Leyendo el HTML del noticiero 20Mintutos... <-----");
+        Newspaper veinteMinutos = newsPaperService.findByName("20Minutos");
+        DateTimeFormatter dateFormat =
+                DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.forLanguageTag("es-ES"));
+        List<Article> newsToday = new ArrayList<>();
 
         try {
-          Document newsDoc = Jsoup.connect(urlNews).get();
-          Element headline = newsDoc.selectFirst("h1.c-detail__title");
-          Element category = newsDoc.selectFirst("div.c-detail__category");
-          Element publicationDate = newsDoc.selectFirst("span.c-detail__date");
+            Document doc = Jsoup.connect(url).get();
+            Elements newsItems = doc.select("article");
+            for (Element newsItem : newsItems) {
+                Element urlNewsEl = newsItem.selectFirst("a");
+                if (urlNewsEl == null) continue;
 
-          if (publicationDate == null ||category == null || headline == null) {
-            log.debug("[VeinteMinutosScraper] -> Noticia incompleta, se ignora: {}", urlNews);
-            continue;
-          }
+                String urlNews = urlNewsEl.attr("href");
+                if (urlNews.isBlank()) continue;
 
-          String dateText = publicationDate.text().split("-")[0].trim();
-          LocalDate newsDate = LocalDate.parse(dateText, dateFormat);
-          LocalDate now = LocalDate.now();
+                if (articleService.existsByUrl(urlNews)) {
+                    log.debug("[VeinteMinutosScraper] -> Noticia duplicada, se ignora: {}", urlNews);
+                    continue;
+                }
 
-          if (newsDate.equals(now)) {
-            Article article = new Article(headline.text(), urlNews, category.text(), now, veinteMinutos);
+                try {
+                    if (urlNews.startsWith("/"))
+                        urlNews = "https://20minutos.es".concat(urlNews);
+                    else
+                        continue;
 
-            //Check the article is not duplicated
-            boolean exists = newsToday.stream()
-              .map(Article::getUrl)
-              .anyMatch(urlNews::equals);
+                    Document newsDoc = Jsoup.connect(urlNews).get();
 
-            if (!exists) {
-              newsToday.add(article);
-            } else {
-              log.warn("[VeinteMinutosScraper] -> Duplicated article in source");
+                    Element headline = newsDoc.selectFirst("h1.c-detail__title");
+                    Element category = newsDoc.selectFirst("div.c-detail__category");
+                    Element publicationDate = newsDoc.selectFirst("time");
+
+                    if (publicationDate == null || category == null || headline == null) {
+                        log.debug("[VeinteMinutosScraper] -> Noticia incompleta, se ignora: {}", urlNews);
+                        continue;
+                    }
+
+                    String dateText = publicationDate.text().split("-")[0].trim();
+                    LocalDate newsDate = LocalDate.parse(dateText, dateFormat);
+                    LocalDate now = LocalDate.now();
+
+                    if (newsDate.equals(now)) {
+                        Article article = new Article(headline.text(), urlNews, category.text(), now, veinteMinutos);
+
+                        //Check the article is not duplicated
+                        boolean exists = newsToday.stream()
+                                .map(Article::getUrl)
+                                .anyMatch(urlNews::equals);
+
+                        if (!exists) {
+                            newsToday.add(article);
+                        } else {
+                            log.warn("[VeinteMinutosScraper] -> Duplicated article in source");
+                        }
+                    }
+                } catch (Exception e) {
+                    log.error("[VeinteMinutosScraper] -> Error al obtener el detalle de la noticia: {}", urlNews, e);
+                }
             }
-          }
         } catch (Exception e) {
-          log.error("[VeinteMinutosScraper] -> Error al obtener el detalle de la noticia: {}", url, e);
+            log.error("[VeinteMinutosScraper] -> Error al procesar la página web: " + e.getMessage());
         }
-      }
-    } catch (Exception e) {
-      log.error("[VeinteMinutosScraper] -> Error al procesar la página web: " + e.getMessage());
+
+        log.info("-----> [VeinteMinutosScraper] -> Fin del paso de lectura de news del día <-----");
+
+        return newsToday;
     }
-
-    log.info("-----> [VeinteMinutosScraper] -> Fin del paso de lectura de news del día <-----");
-
-    return newsToday;
-  }
 }
